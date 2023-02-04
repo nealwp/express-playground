a8508a77149b1c1ce19d1dcc6619733c1b46795c
nealwp <mrprestonneal@gmail.com>
2023-02-03 22:24:59 -0800

    add some comments

--- src/routes/math.routes.ts
+++ src/routes/math.routes.ts
@@ -5,8 +5,9 @@ const router = Router()
 router.post('/addfortytwo', (req, res, next) => {
     const { input } = req.body
     if (typeof input !== 'number'){
-        res.sendStatus(400)
+        res.status(400).json({error: "'input' must be a number"})
     }
+
     res.status(200).json({result: 0})
 })
 
--- tests/routes/math-routes.spec.ts
+++ tests/routes/math-routes.spec.ts
@@ -10,7 +10,19 @@ beforeAll(() => {
 })
 
 describe('math router', () => {
+
+    /**
+     * Here we are testing that the route *behaves* as expected, given each possible condition. We
+     * are **NOT** testing that the route is performing the underlying action correctly. We don't
+     * care if `/addfortytwo` actually does the math correctly, only that it can receive an input of
+     * type `{input: number}` and return a result of type `{result: number}`.
+     */
     describe('POST /addfortytwo', () => {
+        /**
+         * A route is responsible for receiving requests and sending appropriate responses. We
+         * don't care what controller/function this route calls, or what inputs said 
+         * controller/function is called with. 
+         */
         it('should return 200 status and result payload if request succeeds', async () => {
             await supertest(server)
                 .post('/addfortytwo')
@@ -21,11 +33,17 @@ describe('math router', () => {
                     expect(typeof res.body.result).toEqual('number')
                 })
         })
+        /**
+         * Here we ensure that if the input is an invalid, we get a 400 response
+         */
         it('should return 400 status if input fails validation', async () => {
             await supertest(server)
                 .post('/addfortytwo')
                 .send({input: 'abcdefg'})
                 .expect(400)
+                .then(res => {
+                    expect(res.body).toEqual({error: "'input' must be a number"})
+                })
         })
     })
 })
